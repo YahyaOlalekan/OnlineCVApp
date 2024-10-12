c26598bd7c4389919f458f1205bc14b61d2b4ee0
YahyaOlalekan <mybluvedcreator@gmail.com>
2024-10-12 09:19:50 +0100

    Add project files.

---
+++ OnlineCVApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using OnlineCVApp.Models;
+using System.Diagnostics;
+
+namespace OnlineCVApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OnlineCVApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineCVApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
